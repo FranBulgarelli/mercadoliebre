5b09af686d35f5bf3b75c150a1d6ff11cc12f761
FranBulgarelli <bulgarellifrancisco@gmail.com>
2021-08-21 02:54:34 -0300

    subo diseño adaptativo

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use(express.static("public"));
 app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
 
 
-app.listen(port,() => console.log("Servidor funcionando en el puerto " + port))
+app.listen(port,() => console.log("Servidor funcionando en http://localhost:" + port))
